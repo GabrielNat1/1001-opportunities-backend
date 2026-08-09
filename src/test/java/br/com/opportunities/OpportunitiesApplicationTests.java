ec8b7f96893512af827bf09071adc6329dd43427
gabrielnat1 <dcbiel846@gmail.com>
2026-08-08 22:09:40 -0300

    feat: initialization repository and create basic configurations

---
+++ src/test/java/br/com/opportunities/OpportunitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.opportunities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpportunitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
